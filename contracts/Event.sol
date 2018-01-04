5d788f1b2872de819aaefbbd89ad713a9989c4ce
adgalad <carlos.25896@gmail.com>
2018-01-04 13:08:02 -0400

    Update Event.sol

--- contracts/Event.sol
+++ contracts/Event.sol
@@ -9,20 +9,21 @@ contract Event {
     bytes32 hashID;
     bytes32 hashDelegate;
   }
-  
 
   mapping (uint32 => Ticket) public tickets;
   uint32 public nSeat;
   uint64 public date;
   string public name;
   string public place;
+  address owner;
 
   // Options
   bool public resell;
   bool public delegate;
 
-  function Event ( string _name,  string _place,  uint64 _date,
-                   uint32 _nSeat, bool   _resell, bool   _delegate) 
+  /* Constructor */
+  function Event (string _name,  string _place,  uint64 _date,
+                  uint32 _nSeat, bool   _resell, bool   _delegate) 
   public {
     name     = _name;
     place    = _place;
@@ -30,8 +31,41 @@ contract Event {
     nSeat    = _nSeat;
     resell   = _resell;
     delegate = _delegate;
-  }  
+    owner    = msg.sender;
+  }    
 
+  /* Operations */ 
+  function buyTicket(bytes32 buyer, uint32 seat, bytes32 delegated) 
+  public canDelegate(delegated) onTime() ownerOnly(){
+    require(tickets[seat].hashID == 0);
+    Ticket memory t;
+    t.hashID       = buyer; 
+    t.hashDelegate = delegated;
+    tickets[seat]  = t;
+  }
+
+  function resellTicket(bytes32 buyer, uint32 seat) 
+  public canResell() onTime() ownerOnly(){
+    require(tickets[seat].hashID != 0 && tickets[seat].hashID != buyer);
+    tickets[seat].hashID = buyer;
+  }
+  
+
+  /* Setters */
+  function changeDate(uint64 newDate) public onTime() ownerOnly(){
+    date = newDate;
+  }
+
+  function changeName (string newName) public onTime() ownerOnly(){
+    name = newName;
+  }
+
+  function changePlace (string newPlace) public onTime() ownerOnly(){
+    place = newPlace;
+  }
+
+
+  /* Modifiers */
   modifier canDelegate(bytes32 delegated) {
     require( (delegated == 0) != delegate);
     _;
@@ -47,23 +81,37 @@ contract Event {
     _;
   }
 
-  function buyTicket(bytes32 owner, uint32 seat, bytes32 delegated) 
-  public canDelegate(delegated) {
-    require(tickets[seat].hashID == 0);
-    Ticket memory t;
-    t.hashID       = owner; 
-    t.hashDelegate = delegated;
-    tickets[seat]  = t;
+  modifier ownerOnly() {
+    owner == msg.sender;
+    _;
   }
 
-  function resellTicket(bytes32 newOwner, uint32 seat) 
-  public canResell() onTime(){
-    require(tickets[seat].hashID != 0 && tickets[seat].hashID != newOwner);
-    tickets[seat].hashID = newOwner;
+}
+
+contract EventPromoter {
+  mapping (address => Event) public events;
+  address owner;
+
+  function EventPromoter(){
+    owner = msg.sender;
   }
 
-  function changeDate(uint64 newDate) public onTime(){
-    date = newDate;
+  function createEvent (string name,  string place,  uint64 date,
+                        uint32 nSeat, bool   resell, bool   delegate) 
+  public ownerOnly() returns (addres){
+    Event e = new Event(name, place, date, nSeat, resell, delegate);
+    events[address(e)] = e;
+    return address(e);
+  }
+
+  function deleteEvent (address addr) public ownerOnly(){
+    delete events[addr];
+  }
+
+  modifier ownerOnly() {
+    owner == msg.sender;
+    _;
   }
 }
+
 
