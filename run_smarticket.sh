ec02952216f30f9b913fab1e5b1aeb0236ecfa7f
Carlos Spaggiari Roa <carlos.25896@gmail.com>
2019-08-27 15:51:24 -0400

    sleep despues app.js server

--- run_smarticket.sh
+++ run_smarticket.sh
@@ -2,8 +2,9 @@ cd Back
 node app.js eth &
 node app.js server &
 
+sleep 5
 cd ../Front
 cd pyserver
 python3 main.py &
 cd ..
-ng serve --port 80 --host=0.0.0.0 --disable-host-check &
+sudo ng serve --port 80 --host=0.0.0.0 --disable-host-check &
