36333355a0685c58f393ba39b0baa2b418b88379
Carlos Spaggiari Roa <carlos.25896@gmail.com>
2019-08-29 15:03:20 -0400

    try catch en authmiddleware

--- Back/api_server/controller/authentication.js
+++ Back/api_server/controller/authentication.js
@@ -54,8 +54,12 @@ const authMiddleware = function (req, res, next, modifier) {
             if (req.body.token) delete req.body.token
             if (req.query.token) delete req.query.token
             if (req.headers['x-access-token']) delete req.headers['x-access-token']
+            try {
+              return next()
+            } catch (e) {
+              console.log(e)
+            }
 
-            next()
           })
       }
     })
